09a1481064a88b65a6a6492e858278389fffea35
Thuan Pham <thuanowa@gmail.com>
2023-04-22 10:04:23 +0700

    feat(theme): rose pine color support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,8 @@ import compress from "astro-compress";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
   site: "http://thuanowa.com",
   integrations: [tailwind(), compress(), robotsTxt(), sitemap()],
-  output: "server",
-  adapter: netlify()
 });
