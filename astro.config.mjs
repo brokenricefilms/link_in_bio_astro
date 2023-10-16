73fa6819d2d4ba8c9dc7762babdf70e1601b13a8
Thuan Pham <thuanowa@gmail.com>
2023-10-16 10:37:17 +0700

    fix: github build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 
@@ -8,5 +7,5 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://thuanowa.github.io",
   base: "/link_in_bio_astro"
-  integrations: [tailwind(), compress(), robotsTxt(), sitemap()],
+  integrations: [tailwind(), robotsTxt(), sitemap()],
 });
