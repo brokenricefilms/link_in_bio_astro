117b0631029bafd5f27f55c75a60a39f1eec5011
Thuan Pham <thuanowa@gmail.com>
2023-10-16 10:39:49 +0700

    fix: github build

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://thuanowa.github.io",
-  base: "/link_in_bio_astro"
+  base: "/link_in_bio_astro",
   integrations: [tailwind(), robotsTxt(), sitemap()],
 });
